3c01966d6f33e969ce747aeecf4cc18f35e66428
HelloFinger <ddammo55@naver.com>
2020-02-28 18:29:03 +0900

    first

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        <p>{{ greeting }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data:function(){
+            return{
+                greeting:'Hello'
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+
+
+<style>
+p{
+    font-size: 2em;
+    color: tomato;
+    text-align: center;
+}
+</style>
---
+++ resources/js/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <nav class="navbar bg-white border-bottom navbar-light">
+       <router-link class="navbar-brand mr-auto" to="/">LaravelBnb</router-link><br>
+       <router-link class="btn nav-button" to="/second">Second</router-link>
+      </nav>
+
+      <div class="container mt-4 mb-4 pr-4 pl-4"></div>
+
+       <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
